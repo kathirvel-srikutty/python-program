ce2a32e59b142b383878b896c7b8bfeda13c26b7
kathirvel-srikutty <42996720+kathirvel-srikutty@users.noreply.github.com>
2019-02-11 10:59:12 +0530

    Update vowels.c

--- vowels.c
+++ vowels.c
@@ -3,8 +3,6 @@ int main()
 {
     char c;
     int isLowercaseVowel, isUppercaseVowel;
-
-    printf("Enter an alphabet: ");
     scanf("%c",&c);
     isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
     isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
