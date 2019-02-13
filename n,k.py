ec1a49c7f8afab5ce354714b11227ef738faec80
kathirvel-srikutty <42996720+kathirvel-srikutty@users.noreply.github.com>
2019-02-13 14:54:19 +0530

    Create n,k.py

---
+++ n,k.py
@@ -0,0 +1,11 @@
+int findSum(int N, int K) 
+{ 
+    int ans = 0; 
+  
+    // Iterate from 1 to N && 
+    // evaluating and adding i % K. 
+    for (int i = 1; i <= N; i++) 
+        ans += (i % K); 
+  
+    return ans; 
+} 
