98dce1db08b6109a283db9fe00efd3b2adca057e
kathirvel-srikutty <42996720+kathirvel-srikutty@users.noreply.github.com>
2019-02-18 10:42:07 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+# Program to check if a string
+#  is palindrome or not
+# change this value for a different output
+my_str = 'aIbohPhoBiA'
+my_str = my_str.casefold()
+# reverse the string
+rev_str = reversed(my_str)
+# check if the string is equal to its reverse
+if list(my_str) == list(rev_str):
+   print("It is palindrome")
+else:
+   print("It is not palindrome")
